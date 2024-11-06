edda5e7fd698a98b89cb1e73e1a91d2c94d98c0b
chyezh <chyezh@outlook.com>
2024-11-06 13:32:22 +0800

    fix: proxy test use localhost
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -365,6 +365,15 @@ func TestProxy(t *testing.T) {
 	var err error
 	var wg sync.WaitGroup
 	paramtable.Init()
+	params := paramtable.Get()
+	params.ProxyGrpcServerCfg.IP = "localhost"
+	params.RootCoordGrpcServerCfg.IP = "localhost"
+	params.QueryCoordGrpcServerCfg.IP = "localhost"
+	params.DataCoordGrpcServerCfg.IP = "localhost"
+	params.QueryNodeGrpcServerCfg.IP = "localhost"
+	params.IndexNodeGrpcServerCfg.IP = "localhost"
+	params.StreamingNodeGrpcServerCfg.IP = "localhost"
+	params.DataNodeGrpcServerCfg.IP = "localhost"
 
 	path := "/tmp/milvus/rocksmq" + funcutil.GenRandomStr()
 	t.Setenv("ROCKSMQ_PATH", path)
