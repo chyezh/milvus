3064fac88521893ecd683ba6676b4559fdee1100
chyezh <chyezh@outlook.com>
2025-08-19 10:56:23 +0800

    enhance: move L0 allocation before wal write and control the L0 flush policy with streamingnode
    - control the L0 segment allocation before delete operation.
    - control the L0 flush policy at shard manager of streamingnode.
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/streamingnode/server/wal/interceptors/shard/shards/segment_alloc_worker.go
+++ internal/streamingnode/server/wal/interceptors/shard/shards/segment_alloc_worker.go
@@ -17,19 +17,19 @@ import (
 )
 
 // asyncAllocSegment allocates a new growing segment asynchronously.
-func (m *partitionManager) asyncAllocSegment() {
-	if m.onAllocating != nil {
-		m.Logger().Debug("segment alloc worker is already on allocating")
-		// manager is already on allocating.
+func (m *partitionManager) asyncAllocSegment(lv datapb.SegmentLevel) {
+	// Create a notifier to notify the waiter when the allocation is done.
+	if !m.onAllocatingNotifier.Start(lv) {
+		m.Logger().Debug("segment alloc worker is already on allocating", zap.Stringer("level", lv))
 		return
 	}
-	// Create a notifier to notify the waiter when the allocation is done.
-	m.onAllocating = make(chan struct{})
+
 	w := &segmentAllocWorker{
 		ctx:          m.ctx,
 		collectionID: m.collectionID,
 		partitionID:  m.partitionID,
 		vchannel:     m.vchannel,
+		level:        lv,
 		wal:          m.wal.Get(),
 	}
 	w.SetLogger(m.Logger())
@@ -45,6 +45,7 @@ type segmentAllocWorker struct {
 	collectionID int64
 	partitionID  int64
 	vchannel     string
+	level        datapb.SegmentLevel
 	wal          wal.WAL
 	msg          message.MutableMessage
 }
@@ -119,7 +120,7 @@ func (w *segmentAllocWorker) generateNewGrowingSegmentMessage() error {
 			StorageVersion: storage.StorageV2,
 			MaxRows:        limitation.SegmentRows,
 			MaxSegmentSize: limitation.SegmentSize,
-			Level:          datapb.SegmentLevel_L1,
+			Level:          w.level,
 		}).
 		WithBody(&message.CreateSegmentMessageBody{}).
 		MustBuildMutable()
