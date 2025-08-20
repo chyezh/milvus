f802744ab0ece30032bf5c4cf4512b519ce3b2f4
chyezh <chyezh@outlook.com>
2025-08-20 20:26:07 +0800

    enhance: broadcaster will lock resource until message acked
    - Return LastConfirmedMessageID when wal append operation.
    - Add resource-key-based locker for broadcast-ack operation to protect the coord state when executing ddl.
    - Resource-key-based locker is held until the broadcast operation is acked.
    - Add FastAck execute ack right away after the broadcast done to speed up ddl.
    - Ack callback will support multi-message now.
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- pkg/streaming/util/message/broadcast.go
+++ pkg/streaming/util/message/broadcast.go
@@ -1,10 +1,9 @@
 package message
 
 import (
-	"strconv"
-
 	"github.com/milvus-io/milvus/pkg/v2/proto/messagespb"
 	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
+	"google.golang.org/protobuf/proto"
 )
 
 // newBroadcastHeaderFromProto creates a BroadcastHeader from proto.
@@ -26,44 +25,15 @@ type BroadcastHeader struct {
 	ResourceKeys typeutil.Set[ResourceKey]
 }
 
-// NewResourceKeyFromProto creates a ResourceKey from proto.
-func NewResourceKeyFromProto(proto *messagespb.ResourceKey) ResourceKey {
-	return ResourceKey{
-		Domain: proto.Domain,
-		Key:    proto.Key,
-	}
+// BroadcastResult is the result of broadcast operation.
+type BroadcastResult[H proto.Message, B proto.Message] struct {
+	Message SpecializedBroadcastMessage[H, B]
+	Results map[string]*AppendResult
 }
 
-// newProtoFromResourceKey creates a set of proto from ResourceKey.
-func newProtoFromResourceKey(keys ...ResourceKey) []*messagespb.ResourceKey {
-	deduplicated := typeutil.NewSet(keys...)
-	protos := make([]*messagespb.ResourceKey, 0, len(keys))
-	for key := range deduplicated {
-		protos = append(protos, &messagespb.ResourceKey{
-			Domain: key.Domain,
-			Key:    key.Key,
-		})
-	}
-	return protos
-}
-
-type ResourceKey struct {
-	Domain messagespb.ResourceDomain
-	Key    string
-}
-
-// NewImportJobIDResourceKey creates a key for import job resource.
-func NewImportJobIDResourceKey(importJobID int64) ResourceKey {
-	return ResourceKey{
-		Domain: messagespb.ResourceDomain_ResourceDomainImportJobID,
-		Key:    strconv.FormatInt(importJobID, 10),
-	}
-}
-
-// NewCollectionNameResourceKey creates a key for collection name resource.
-func NewCollectionNameResourceKey(collectionName string) ResourceKey {
-	return ResourceKey{
-		Domain: messagespb.ResourceDomain_ResourceDomainCollectionName,
-		Key:    collectionName,
-	}
+// AppendResult is the result of append operation.
+type AppendResult struct {
+	MessageID              MessageID
+	LastConfirmedMessageID MessageID
+	TimeTick               uint64
 }
