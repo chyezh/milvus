edd5d1bd06ef42fecff40be687c3329ff619cda8
chyezh <chyezh@outlook.com>
2025-08-25 14:40:56 +0800

    temp storage
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/datacoord/handler_test.go
+++ internal/datacoord/handler_test.go
@@ -277,9 +277,9 @@ func TestGetQueryVChanPositions(t *testing.T) {
 		vchannel := "ch_no_segment_1"
 		pchannel := funcutil.ToPhysicalChannel(vchannel)
 		infos := svr.handler.GetQueryVChanPositions(&channelMeta{
-			Name:           vchannel,
-			CollectionID:   0,
-			StartPositions: []*commonpb.KeyDataPair{{Key: pchannel, Data: []byte{14, 15, 16}}},
+			Name:          vchannel,
+			CollectionID:  0,
+			StartPosition: []*commonpb.KeyDataPair{{Key: pchannel, Data: []byte{14, 15, 16}}},
 		})
 		assert.EqualValues(t, 0, infos.CollectionID)
 		assert.EqualValues(t, vchannel, infos.ChannelName)
@@ -1502,9 +1502,9 @@ func TestGetDataVChanPositions(t *testing.T) {
 		vchannel := "ch_no_segment_1"
 		pchannel := funcutil.ToPhysicalChannel(vchannel)
 		infos := svr.handler.GetDataVChanPositions(&channelMeta{
-			Name:           vchannel,
-			CollectionID:   0,
-			StartPositions: []*commonpb.KeyDataPair{{Key: pchannel, Data: []byte{14, 15, 16}}},
+			Name:          vchannel,
+			CollectionID:  0,
+			StartPosition: []*commonpb.KeyDataPair{{Key: pchannel, Data: []byte{14, 15, 16}}},
 		}, allPartitionID)
 		assert.EqualValues(t, 0, infos.CollectionID)
 		assert.EqualValues(t, vchannel, infos.ChannelName)
