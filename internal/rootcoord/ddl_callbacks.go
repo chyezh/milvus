42de8bb5224977fc10f1acb58fcc50a9b94d8dcd
chyezh <chyezh@outlook.com>
2025-08-27 15:32:02 +0800

    enhance: support alter collection for wal
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/rootcoord/ddl_callbacks.go
+++ internal/rootcoord/ddl_callbacks.go
@@ -56,6 +56,7 @@ func (c *DDLCallback) registerDatabaseCallbacks() {
 func (c *DDLCallback) registerCollectionCallbacks() {
 	registry.RegisterCreateCollectionV1AckCallback(c.createCollectionV1AckCallback)
 	registry.RegisterDropCollectionV1AckCallback(c.dropCollectionV1AckCallback)
+	registry.RegisterAlterCollectionV2AckCallback(c.putCollectionV2AckCallback)
 }
 
 // registerPartitionCallbacks registers the partition callbacks.
