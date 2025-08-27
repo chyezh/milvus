40d452baa2810a43418ee8c45f2c2b3218809a1a
chyezh <chyezh@outlook.com>
2025-08-27 15:32:02 +0800

    enhance: support alter collection for wal
    Signed-off-by: chyezh <chyezh@outlook.com>

---
+++ internal/rootcoord/ddl_callbacks_add_collection_field.go
@@ -0,0 +1,96 @@
+package rootcoord
+
+import (
+	"context"
+
+	"github.com/cockroachdb/errors"
+	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
+	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
+	"github.com/milvus-io/milvus/internal/distributed/streaming"
+	"github.com/milvus-io/milvus/internal/metastore/model"
+	"github.com/milvus-io/milvus/internal/streamingcoord/server/broadcaster/broadcast"
+	"github.com/milvus-io/milvus/pkg/v2/proto/etcdpb"
+	"github.com/milvus-io/milvus/pkg/v2/proto/messagespb"
+	"github.com/milvus-io/milvus/pkg/v2/streaming/util/message"
+	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+// broadcastPutCollectionV2ForAddField broadcasts the put collection v2 message for add field.
+func (c *Core) broadcastPutCollectionV2ForAddField(ctx context.Context, req *milvuspb.AddCollectionFieldRequest) error {
+	broadcaster, err := broadcast.StartBroadcastWithResourceKeys(ctx,
+		message.NewSharedDBNameResourceKey(req.GetDbName()),
+		message.NewExclusiveCollectionNameResourceKey(req.GetDbName(), req.GetCollectionName()),
+	)
+	if err != nil {
+		return err
+	}
+
+	// check if the collection is created.
+	coll, err := c.meta.GetCollectionByName(ctx, req.GetDbName(), req.GetCollectionName(), typeutil.MaxTimestamp)
+	if err != nil {
+		return err
+	}
+	if coll.State != etcdpb.CollectionState_CollectionCreated {
+		return errors.Errorf("collection is not created, can not add field, state: %s", coll.State.String())
+	}
+
+	// check if the field schema is illegal.
+	fieldSchema := &schemapb.FieldSchema{}
+	if err = proto.Unmarshal(req.Schema, fieldSchema); err != nil {
+		return errors.Wrap(err, "failed to unmarshal field schema")
+	}
+	if err := checkFieldSchema([]*schemapb.FieldSchema{fieldSchema}); err != nil {
+		return errors.Wrap(err, "failed to check field schema")
+	}
+	// check if the field already exists
+	for _, field := range coll.Fields {
+		if field.Name == fieldSchema.Name {
+			// TODO: idempotency check here.
+			return errors.Errorf("field already exists, name: %s", fieldSchema.Name)
+		}
+	}
+
+	// assign a new field id.
+	fieldSchema.FieldID = nextFieldID(coll)
+
+	// build new collection schema.
+	schema := &schemapb.CollectionSchema{
+		Name:               coll.Name,
+		Description:        coll.Description,
+		AutoID:             coll.AutoID,
+		Fields:             model.MarshalFieldModels(coll.Fields),
+		StructArrayFields:  model.MarshalStructArrayFieldModels(coll.StructArrayFields),
+		Functions:          model.MarshalFunctionModels(coll.Functions),
+		EnableDynamicField: coll.EnableDynamicField,
+		Properties:         coll.Properties,
+	}
+	schema.Fields = append(schema.Fields, fieldSchema)
+
+	channels := make([]string, 0, len(coll.VirtualChannelNames)+1)
+	channels = append(channels, streaming.WAL().ControlChannel())
+	for _, vchannel := range coll.VirtualChannelNames {
+		channels = append(channels, vchannel)
+	}
+	// broadcast the put collection v2 message.
+	msg := message.NewPutCollectionMessageBuilderV2().
+		WithHeader(&messagespb.PutCollectionMessageHeader{
+			DbId:         coll.DBID,
+			CollectionId: coll.CollectionID,
+			UpdateMask: &fieldmaskpb.FieldMask{
+				Paths: []string{message.FieldMaskCollectionSchema},
+			},
+		}).
+		WithBody(&messagespb.PutCollectionMessageBody{
+			Updates: &messagespb.PutCollectionMessageUpdates{
+				Schema: schema,
+			},
+		}).
+		WithBroadcast(channels).
+		MustBuildBroadcast()
+	if _, err := broadcaster.Broadcast(ctx, msg); err != nil {
+		return err
+	}
+	return nil
+}
