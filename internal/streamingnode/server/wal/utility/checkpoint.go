a9cddda581486f8c8a4aadfd18cf8877b0c595b8
chyezh <chyezh@outlook.com>
2025-09-10 15:31:33 +0800

    fix: loop message when switch primary
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/streamingnode/server/wal/utility/checkpoint.go
+++ internal/streamingnode/server/wal/utility/checkpoint.go
@@ -20,16 +20,7 @@ func NewWALCheckpointFromProto(cp *streamingpb.WALCheckpoint) *WALCheckpoint {
 		ReplicateConfig: cp.ReplicateConfig,
 	}
 	if cp.ReplicateCheckpoint != nil {
-		var messageID message.MessageID
-		if cp.ReplicateCheckpoint.MessageId != nil {
-			messageID = message.MustUnmarshalMessageID(cp.ReplicateCheckpoint.MessageId)
-		}
-		wcp.ReplicateCheckpoint = &ReplicateCheckpoint{
-			ClusterID: cp.ReplicateCheckpoint.ClusterId,
-			PChannel:  cp.ReplicateCheckpoint.Pchannel,
-			MessageID: messageID,
-			TimeTick:  cp.ReplicateCheckpoint.TimeTick,
-		}
+		wcp.ReplicateCheckpoint = NewReplicateCheckpointFromProto(cp.ReplicateCheckpoint)
 	}
 	return wcp
 }
@@ -70,10 +61,14 @@ func (c *WALCheckpoint) Clone() *WALCheckpoint {
 
 // NewReplicateCheckpointFromProto creates a new ReplicateCheckpoint from a protobuf message.
 func NewReplicateCheckpointFromProto(cp *commonpb.ReplicateCheckpoint) *ReplicateCheckpoint {
+	var msgID message.MessageID
+	if cp.MessageId != nil {
+		msgID = message.MustUnmarshalMessageID(cp.MessageId)
+	}
 	return &ReplicateCheckpoint{
 		ClusterID: cp.ClusterId,
 		PChannel:  cp.Pchannel,
-		MessageID: message.MustUnmarshalMessageID(cp.MessageId),
+		MessageID: msgID,
 		TimeTick:  cp.TimeTick,
 	}
 }
