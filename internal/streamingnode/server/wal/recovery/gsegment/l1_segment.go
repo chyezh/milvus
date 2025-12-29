3fe1e2a06f1afdba2f080831ba3c31f34fac567e
chyezh <chyezh@outlook.com>
2025-12-29 19:45:47 +0800

    temp storage
    Signed-off-by: chyezh <chyezh@outlook.com>

---
+++ internal/streamingnode/server/wal/recovery/gsegment/l1_segment.go
@@ -0,0 +1,142 @@
+// Licensed to the LF AI & Data foundation under one
+// or more contributor license agreements. See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership. The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gsegment
+
+import (
+	"sync"
+
+	"github.com/milvus-io/milvus/pkg/v2/proto/streamingpb"
+	"github.com/milvus-io/milvus/pkg/v2/streaming/util/message"
+	"github.com/milvus-io/milvus/pkg/v2/util/paramtable"
+	"github.com/milvus-io/milvus/pkg/v2/util/tsoutil"
+)
+
+// L1Segment is the segment recovery info for L1 segment.
+type L1Segment struct {
+	mu sync.Mutex
+
+	meta         *streamingpb.SegmentAssignmentMeta
+	sealed       bool           // whether the segment is sealed.
+	sealedChunks []*InsertChunk // sealed chunks for insert messages, the data in the chunk will never be changed.
+	growingChunk *InsertChunk   // growing chunk for insert messages, the data in the chunk can be changed.
+	dirty        bool           // whether the segment recovery info is dirty.
+}
+
+// ObserveInsert observes an insert message.
+func (s *L1Segment) ObserveInsert(msg message.ImmutableInsertMessageV1) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	timetick := msg.TimeTick()
+	if timetick < s.meta.CheckpointTimeTick {
+		return
+	}
+	// current we only support one partition per insert message.
+	// TODO: after we support multiple partitions per insert message, we need to update this codes.
+	rows := msg.Header().TotolRows()
+	binarySize := uint64(msg.EstimateSize())
+
+	s.pushMessageIntoChunk(msg, binarySize)
+	s.meta.Stat.ModifiedBinarySize += binarySize
+	s.meta.Stat.ModifiedRows += rows
+	s.meta.Stat.LastModifiedTimestamp = tsoutil.PhysicalTime(timetick).Unix()
+	if s.meta.Stat.BeginTimeTick == 0 {
+		s.meta.Stat.BeginTimeTick = timetick
+		s.meta.Stat.EndTimeTick = timetick
+	}
+	s.meta.Stat.EndTimeTick = timetick
+	s.meta.CheckpointTimeTick = timetick
+	s.dirty = true
+}
+
+// Flush flushes the segment from growing state into flushed state.
+func (s *L1Segment) Flush(timetick uint64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if timetick < s.meta.CheckpointTimeTick {
+		return
+	}
+
+	if s.sealed {
+		return
+	}
+	s.sealed = true
+	s.sealGrowingChunk()
+	s.meta.CheckpointTimeTick = timetick
+	s.meta.Stat.FlushSegmentTimeTick = timetick
+	s.dirty = true
+}
+
+// BeginSaveChunk begins to save the chunk.
+func (s *L1Segment) BeginSaveChunk() (chunk *InsertChunk, ok bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if len(s.sealedChunks) == 0 {
+		return nil, false
+	}
+	return s.sealedChunks[0], true
+}
+
+type SaveChunkDoneRequest struct {
+	ManifestPath string
+	Binlog       *streamingpb.L1SegmentBinLogs
+}
+
+// SaveChunkDone saves the chunk and pops it from the sealed chunks.
+func (s *L1Segment) SaveChunkDone(req *SaveChunkDoneRequest) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.meta.Stat.BinlogCounter++
+	l1Storage := s.meta.PersistedStorage.(*streamingpb.SegmentAssignmentMeta_L1).L1
+	l1Storage.ManifestPath = req.ManifestPath
+	l1Storage.Binlogs = append(l1Storage.Binlogs, req.Binlog)
+	s.sealedChunks = s.sealedChunks[1:]
+	if len(s.sealedChunks) == 0 && s.sealed {
+		// if the segment is already flushed, the L1 segment is not needed to be saved anymore.
+		s.meta.State = streamingpb.SegmentAssignmentState_SEGMENT_ASSIGNMENT_STATE_FLUSHED
+	}
+	s.dirty = true
+}
+
+// pushMessageIntoChunk pushes the message into the chunk.
+func (s *L1Segment) pushMessageIntoChunk(msg message.ImmutableInsertMessageV1, size uint64) {
+	if s.growingChunk != nil && s.growingChunk.AvailableSize() < int64(size) {
+		// if the growing chunk is not enough to hold the message, seal it right away.
+		s.sealGrowingChunk()
+	}
+	if s.growingChunk == nil {
+		expectedChunkSize := paramtable.Get().DataNodeCfg.FlushInsertBufferSize.GetAsInt64()
+		s.growingChunk = newInsertChunk(expectedChunkSize)
+	}
+	s.growingChunk.Push(msg)
+	if s.growingChunk.AvailableSize() <= 0 {
+		// may be a single message is greater than the expected chunk size, so we need to seal the growing chunk right now.
+		s.sealGrowingChunk()
+	}
+}
+
+// sealGrowingChunk seals the growing chunk and adds it to the sealed chunks.
+func (s *L1Segment) sealGrowingChunk() {
+	if s.growingChunk == nil {
+		return
+	}
+	s.sealedChunks = append(s.sealedChunks, s.growingChunk)
+	s.growingChunk = nil
+}
---
+++ internal/streamingnode/server/wal/recovery/gsegment/sync_scheduler.go
@@ -0,0 +1,138 @@
+package gsegment
+
+import (
+	"container/list"
+	"context"
+	"sync"
+
+	"go.uber.org/atomic"
+)
+
+// SyncScheduler is a scheduler that can schedule tasks in a CPU-bounded and IO-bounded background.
+type SyncScheduler struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
+
+	cpuLimit *atomic.Int32
+	ioLimit  *atomic.Int32
+
+	cond       *sync.Cond
+	cpuTasks   *list.List
+	ioTasks    *list.List
+	cpuRunning int32
+	ioRunning  int32
+}
+
+// NewSyncScheduler creates a new SyncScheduler.
+func NewSyncScheduler(cpuLimit, ioLimit int) *SyncScheduler {
+	ctx, cancel := context.WithCancel(context.Background())
+	s := &SyncScheduler{
+		ctx:      ctx,
+		cancel:   cancel,
+		cpuLimit: atomic.NewInt32(int32(cpuLimit)),
+		ioLimit:  atomic.NewInt32(int32(ioLimit)),
+		cpuTasks: list.New(),
+		ioTasks:  list.New(),
+	}
+	s.cond = sync.NewCond(&sync.Mutex{})
+	s.wg.Add(1)
+	go s.scheduleLoop()
+	return s
+}
+
+// AddTask adds a task to the scheduler.
+func (s *SyncScheduler) AddTask(task SaveChunkTask) {
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+
+	if task.CPUBound() {
+		s.cpuTasks.PushBack(task)
+	} else {
+		s.ioTasks.PushBack(task)
+	}
+	s.cond.Signal()
+}
+
+// UpdateLimits updates the limits of the scheduler.
+func (s *SyncScheduler) UpdateLimits(cpuLimit, ioLimit int) {
+	s.cpuLimit.Store(int32(cpuLimit))
+	s.ioLimit.Store(int32(ioLimit))
+
+	s.cond.L.Lock()
+	s.cond.Broadcast()
+	s.cond.L.Unlock()
+}
+
+// scheduleLoop is the main loop of the scheduler.
+func (s *SyncScheduler) scheduleLoop() {
+	defer s.wg.Done()
+	for {
+		s.cond.L.Lock()
+		for s.ctx.Err() == nil &&
+			(s.cpuRunning >= s.cpuLimit.Load() || s.cpuTasks.Len() == 0) &&
+			(s.ioRunning >= s.ioLimit.Load() || s.ioTasks.Len() == 0) {
+			s.cond.Wait()
+		}
+
+		if s.ctx.Err() != nil {
+			s.cond.L.Unlock()
+			return
+		}
+
+		// Dispatch CPU tasks
+		for s.cpuRunning < s.cpuLimit.Load() && s.cpuTasks.Len() > 0 {
+			element := s.cpuTasks.Front()
+			s.cpuTasks.Remove(element)
+			task := element.Value.(SaveChunkTask)
+			s.cpuRunning++
+			s.wg.Add(1)
+			go s.executeTask(task, true)
+		}
+
+		// Dispatch IO tasks
+		for s.ioRunning < s.ioLimit.Load() && s.ioTasks.Len() > 0 {
+			element := s.ioTasks.Front()
+			s.ioTasks.Remove(element)
+			task := element.Value.(SaveChunkTask)
+			s.ioRunning++
+			s.wg.Add(1)
+			go s.executeTask(task, false)
+		}
+		s.cond.L.Unlock()
+	}
+}
+
+// executeTask executes a task.
+func (s *SyncScheduler) executeTask(task SaveChunkTask, wasCPU bool) {
+	defer s.wg.Done()
+
+	err := task.Poll(s.ctx)
+
+	s.cond.L.Lock()
+	defer s.cond.L.Unlock()
+	if wasCPU {
+		s.cpuRunning--
+	} else {
+		s.ioRunning--
+	}
+
+	if err != nil {
+		// if the task is not finished, put it back to the head of the queue to continue polling.
+		if task.CPUBound() {
+			s.cpuTasks.PushFront(task)
+		} else {
+			s.ioTasks.PushFront(task)
+		}
+	}
+	s.cond.Signal()
+}
+
+// Close closes the scheduler.
+func (s *SyncScheduler) Close() {
+	s.cancel()
+	s.cond.L.Lock()
+	s.cond.Broadcast()
+	s.cond.L.Unlock()
+	s.wg.Wait()
+}
