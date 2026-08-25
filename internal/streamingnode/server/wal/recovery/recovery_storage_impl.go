e2e4e0dec1d773764524aa7a1e27b22f8e728930
chyezh <chyezh@outlook.com>
2026-08-25 20:02:43 +0800

    style: gofmt the outbox changes (#52542)
    Signed-off-by: chyezh <chyezh@outlook.com>

--- internal/streamingnode/server/wal/recovery/recovery_storage_impl.go
+++ internal/streamingnode/server/wal/recovery/recovery_storage_impl.go
@@ -28,11 +28,11 @@ import (
 	"github.com/milvus-io/milvus/pkg/v3/streaming/util/message"
 	"github.com/milvus-io/milvus/pkg/v3/streaming/util/types"
 	"github.com/milvus-io/milvus/pkg/v3/streaming/walimpls"
+	"github.com/milvus-io/milvus/pkg/v3/util/commonpbutil"
 	"github.com/milvus-io/milvus/pkg/v3/util/funcutil"
 	"github.com/milvus-io/milvus/pkg/v3/util/merr"
 	"github.com/milvus-io/milvus/pkg/v3/util/nodescheduler"
 	"github.com/milvus-io/milvus/pkg/v3/util/paramtable"
-	"github.com/milvus-io/milvus/pkg/v3/util/commonpbutil"
 	"github.com/milvus-io/milvus/pkg/v3/util/replicateutil"
 	"github.com/milvus-io/milvus/pkg/v3/util/syncutil"
 )
